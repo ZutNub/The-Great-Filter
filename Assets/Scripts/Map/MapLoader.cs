60abbde5215fdd7ca8458774de080536910ed476
Andreas Hanuja <hanuja2@gmail.com>
2021-05-30 20:39:24 +0200

    push push

--- Assets/Scripts/Map/MapLoader.cs
+++ Assets/Scripts/Map/MapLoader.cs
@@ -9,7 +9,7 @@ namespace GreatFilter.Map
     public class MapLoader : MonoBehaviour
     {
         public DataLevel level;
-        public GameObject planetPrefab;
+        public GameObject planetPreviewObject;
 
         [Button("Reload")]
         public void Reload()
@@ -20,21 +20,40 @@ namespace GreatFilter.Map
 
         private void Cleanup()
         {
-            foreach(Transform t in transform)
+            if (Application.isPlaying)
             {
-                if (Application.isPlaying)
+                foreach (Transform t in transform)
                 {
                     Destroy(t.gameObject);
                 }
-                else
-                {
-                    DestroyImmediate(t.gameObject);
-                }
             }
+            else
+            {
+                while(transform.childCount > 0)
+                {
+                    DestroyImmediate(transform.GetChild(0).gameObject);
+                }
+            }              
         }
         private void Load()
         {
+            List<GameObject> planets = new List<GameObject>();
+            for(int i=0; i < level.planets.Count; i++)
+            {
+                DataLevel.PlanetSetup p = level.planets[i];
 
+                GameObject planetObject = GameObject.Instantiate(planetPreviewObject, transform);
+                planets.Add(planetObject);
+
+                planetObject.name = "Planet " + i;
+
+                Vector3 origin = Vector3.zero;
+                if (p.hasParent)
+                {
+                    origin = planets[p.parent].transform.position;
+                }
+                OrbitCalculator.e(,)
+            }
         }
     }
 }
