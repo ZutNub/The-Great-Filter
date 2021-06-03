c74d46a03b0a55d8b81a3bb6c81e553a119ff506
Andreas Hanuja <hanuja2@gmail.com>
2021-06-03 21:07:29 +0200

    Maploader rotations & turrets

--- Assets/Scripts/Map/MapData/ADataPlanet.cs
+++ Assets/Scripts/Map/MapData/ADataPlanet.cs
@@ -11,12 +11,16 @@ namespace GreatFilter.Map.MapData
         [PreviewField(80)] [HideLabel] public Sprite sprite;
 
         [VerticalGroup("PlanetData/Stats")] [Tooltip("the relative mass of the planet, used for gravitation")] [LabelWidth(70)][Range(100,1000)] public int mass;
-        [VerticalGroup("PlanetData/Stats")] [Tooltip("the size of the planet, affects turret slot ammount")] [LabelWidth(70)] [Range(10, 45)] [OnValueChanged("UpdateSize")] public float size;
+        [VerticalGroup("PlanetData/Stats")] [Tooltip("the size of the planet, affects turret slot ammount")] [LabelWidth(70)] [Range(1, 5)] [OnValueChanged("UpdateSize")] public float size;
         [VerticalGroup("PlanetData/Stats")] [ReadOnly] [LabelWidth(70)] public int turretSlots;
 
         private void UpdateSize()
         {
-            turretSlots = (int)((size * 2 * Mathf.PI) / 30f);
+            turretSlots = GetMaxTurretSlots();
+        }
+        public int GetMaxTurretSlots()
+        {
+            return (int)((size * 2 * Mathf.PI) / 10f + 2); 
         }
     }
 }
--- Assets/Scripts/Map/MapLoader.cs
+++ Assets/Scripts/Map/MapLoader.cs
@@ -10,6 +10,8 @@ namespace GreatFilter.Map
     {
         public DataLevel level;
         public GameObject planetPreviewObject;
+        public GameObject turretPreviewObject;
+        public bool reloadInFixedUpdate;
 
         [Button("Reload")]
         public void Reload()
@@ -54,25 +56,61 @@ namespace GreatFilter.Map
                     origin = planets[p.parent].transform.position;
                     axisRatio = 1;
                 }
+                
+                //scale
+                planetObject.transform.localScale = new Vector3(p.planet.size, p.planet.size, p.planet.size);
+
+                //position & orbit
                 List<Vector3> orbit = OrbitCalculator.CalculateEllipse(origin, axisRatio * p.distance, p.distance, 0, 0, Mathf.Deg2Rad * 1);
                 planetObject.transform.position = orbit[(int)p.startRotation];
-                planetObject.transform.GetChild(0).position = orbit[(int)(p.startRotation+p.axisRotation)%360];
+                planetObject.transform.GetChild(0).position = orbit[(int)(p.startRotation+p.oribitalRotation) %360];
 
+                //rotation
+                planetObject.transform.GetChild(0).localRotation = Quaternion.Euler(0, 0, p.axisRotation);
+
+                //dottet line
                 LineRenderer dottedLine = planetObject.transform.GetChild(0).GetComponent<LineRenderer>();
                 List<Vector3> dottedPoints = new List<Vector3>();
-                if(p.startRotation + p.axisRotation > 360)
+                if(p.startRotation + p.oribitalRotation > 360)
                 {
                     dottedPoints.AddRange(orbit.GetRange((int)p.startRotation, orbit.Count- (int)p.startRotation));
-                    dottedPoints.AddRange(orbit.GetRange(0, (int)(p.axisRotation - dottedPoints.Count)));
+                    dottedPoints.AddRange(orbit.GetRange(0, (int)(p.oribitalRotation - dottedPoints.Count)));
                 }
                 else
                 {
-                    dottedPoints.AddRange(orbit.GetRange((int)p.startRotation, (int)p.axisRotation));
+                    dottedPoints.AddRange(orbit.GetRange((int)p.startRotation, (int)p.oribitalRotation));
                 }
-
                 dottedLine.positionCount = dottedPoints.Count;
                 dottedLine.SetPositions(dottedPoints.ToArray());
+
+                //turrets
+                if (p.turrets != null)
+                {
+                    foreach (var t in p.turrets)
+                    {
+                        int index = t.Key;
+                        ADataTurret turret = t.Value;
+
+                        //turret preview on main planet
+                        GameObject turretObject = GameObject.Instantiate(turretPreviewObject, planetObject.transform);
+                        turretObject.transform.localRotation = Quaternion.Euler(0, 0, 360 * (index / (float)p.planet.GetMaxTurretSlots()));
+                        turretObject.transform.GetChild(0).localScale = new Vector3(1f / p.planet.size, 1f / p.planet.size, 1f / p.planet.size);
+                        turretObject.transform.GetChild(0).localPosition = new Vector3(0,0.35f+0.5f/ p.planet.size, 0);
+
+                        //turret preview on preview planet
+                        GameObject turretObjectPreview = GameObject.Instantiate(turretPreviewObject, planetObject.transform.GetChild(0));
+                        turretObjectPreview.transform.localRotation = Quaternion.Euler(0, 0, 360 * (index / (float)p.planet.GetMaxTurretSlots()));
+                        turretObjectPreview.transform.GetChild(0).localScale = new Vector3(1f / p.planet.size, 1f / p.planet.size, 1f / p.planet.size);
+                        turretObjectPreview.transform.GetChild(0).localPosition = new Vector3(0, 0.35f + 0.5f / p.planet.size, 0);
+                    }
+                }
             }
+        }
+
+        private void FixedUpdate()
+        {
+            if(reloadInFixedUpdate)
+                Reload();
         }
     }
 }
